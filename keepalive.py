59e397172ef56d969dacb733ff80dbc5768cd3a5
samwithdoubles <40677402-samwithdoubles@users.noreply.replit.com>
2025-03-20 12:05:34 +0000

    Initial commit

--- keepalive.py
+++ keepalive.py
@@ -5,12 +5,16 @@ from threading import Thread
 
 app = Flask('')
 
+
 @app.route('/')
 def main():
     return "Your bot is alive!\n\nInvite the Demo Bot with this link:\nhttps://discord.com/api/oauth2/authorize?client_id=979560474104463381&permissions=8&scope=bot"
-def run():
-  app.run(host='0.0.0.0',port=8080)
 
-def keep_alive():  
-    server = Thread(target=run)
-    server.start()
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    thread = Thread(target=run)
+    thread.start()
